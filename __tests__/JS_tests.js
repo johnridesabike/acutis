00ef1e07054508af8e167f0611a571b8a0696a4e
John <37978984+johnridesabike@users.noreply.github.com>
2021-09-04 18:46:18 -0400

    Relicensed to MPL

--- __tests__/JS_tests.js
+++ __tests__/JS_tests.js
@@ -1,18 +1,10 @@
 /**
- *    Copyright 2021 John Jackson
- *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- */
+  Copyright (c) 2021 John Jackson. 
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 const { Compile, Environment, Result, Source } = require("../");
 
@@ -20,8 +12,10 @@ let emptyComponents = Compile.Components.empty();
 
 describe("The JS interface works as expected", () => {
   test("Bad input is reported correctly", () => {
-    const X = Source.funcWithString("X", 1, (ast) => (env, props, children) =>
-      env.render(ast, props, children)
+    const X = Source.funcWithString(
+      "X",
+      1,
+      (ast) => (env, props, children) => env.render(ast, props, children)
     );
     expect(Compile.make(X, emptyComponents)).toEqual({
       NAME: "errors",
