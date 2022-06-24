d77856dde869aa6ac760b8cf027b09d6f8521e41
John <37978984+johnridesabike@users.noreply.github.com>
2022-06-24 19:31:20 -0400

    Replace ReScript with OCaml.

--- fixtures/cli_test/D.mjs
+++
@@ -1,8 +0,0 @@
-import { Source, Typescheme } from "../../lib/es6/src/AcutisJs.mjs";
-
-export default Source.fn(
-  "D",
-  Typescheme.make([["name", Typescheme.string()]]),
-  Typescheme.Child.make([]),
-  (Env, { name }, _children) => Env.return_("D: " + name.toUpperCase())
-);
--- fixtures/cli_test/bb_async.cjs
+++
@@ -1,8 +0,0 @@
-const { Source, Typescheme } = require("../../");
-
-module.exports = Source.fn(
-  "Bb_async",
-  Typescheme.make([["name", Typescheme.string()]]),
-  Typescheme.Child.make([]),
-  async (Env, { name }, _children) => Env.return_("B: " + name.toUpperCase())
-);
