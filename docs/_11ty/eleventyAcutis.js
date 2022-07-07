000bd7bf2bdc415388a66daf23842f94aadc37c1
John <37978984+johnridesabike@users.noreply.github.com>
2022-07-07 15:18:56 -0400

    Tweak 11ty extension.

--- docs/_11ty/eleventyAcutis.js
+++ docs/_11ty/eleventyAcutis.js
@@ -14,50 +14,68 @@ const { Compile, Render, Utils } = require("../../");
 
 const readFile = util.promisify(fs.readFile);
 
+/*
+ Due to the way JSOO modifies Node.JS's default error handling, in addition to
+ the incompatibilities between OCaml/JSOO exceptions and JS exceptions, we
+ avoid throwing errors that come from the OCaml side.
+
+ We continue to raise any exceptions that come from Eleventy, since Eleventy
+ will handle those itself.
+*/
+
 module.exports = function (eleventyConfig, config) {
   let components = Compile.components([]);
   eleventyConfig.addTemplateFormats("acutis");
   eleventyConfig.addExtension("acutis", {
     read: true,
     data: true,
-    init: async function () {
+    init: function () {
       const glob = path.join(
         this.config.inputDir,
         this.config.dir.includes,
         "**/*.acutis"
       );
-      const files = await fastGlob(glob);
-      try {
-        const queue = await Promise.all(
-          files.map(async (fileName) => {
-            const str = await readFile(fileName, "utf-8");
-            const name = Utils.filenameToComponent(fileName);
-            return Compile.fromString(name, str);
-          })
-        );
-        components = Compile.components([...queue, ...config.components]);
-      } catch (e) {
-        if (Utils.isError(e)) {
-          console.error(Utils.getError(e));
-        } else {
-          throw e;
-        }
-      }
+      return fastGlob(glob)
+        .then((files) => {
+          const filesp = files.map((fileName) =>
+            readFile(fileName, "utf-8").then((src) => {
+              const name = Utils.filenameToComponent(fileName);
+              return Compile.fromString(name, src);
+            })
+          );
+          return Promise.all(filesp);
+        })
+        .then((arr) => {
+          components = Compile.components([...arr, ...config.components]);
+        })
+        .catch((e) => {
+          if (Utils.isError(e)) {
+            console.error(Utils.getError(e));
+          } else {
+            throw e;
+          }
+        });
     },
     compile: function (str, inputPath) {
       try {
         const template = Compile.make(inputPath, components, str);
         return function (data) {
-          return Render.async(template, data);
+          return Render.async(template, data).catch((e) => {
+            if (Utils.isError(e)) {
+              console.error(Utils.getError(e));
+              return "error";
+            } else {
+              throw e;
+            }
+          });
         };
       } catch (e) {
         if (Utils.isError(e)) {
           console.error(Utils.getError(e));
-          return function (data) {
+          return function (_data) {
             return "error";
           };
         } else {
-          console.log(e);
           throw e;
         }
       }
