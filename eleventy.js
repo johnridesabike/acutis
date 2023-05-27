97c8c2b2625f452eb62428e63e52c2eacadd0879
John <37978984+johnridesabike@users.noreply.github.com>
2023-05-27 18:44:55 -0400

    Tweaks

--- eleventy.js
+++ eleventy.js
@@ -6,12 +6,55 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-const path = require("path");
-const fs = require("fs/promises");
-const { Compile, Component, Render, Utils } = require(".");
+let path = require("path");
+let fs = require("fs/promises");
+let { Compile, Component, Render, Utils } = require(".");
 
-const defaultIgnores = new Set([".git"]);
-const globalIgnores = new Set(["node_modules"]);
+function FileWalker(root) {
+  this.ignores = new Set();
+  this.ignores.add(".git");
+  this.ignoresGlobal = new Set();
+  this.ignoresGlobal.add("node_modules");
+  this.root = root;
+}
+
+FileWalker.prototype.ignore = function (filePath) {
+  this.ignores.add(filePath);
+  return this;
+};
+
+FileWalker.prototype._walk = function (filePath, f) {
+  if (this.ignores.has(filePath)) {
+    return Promise.resolve();
+  } else {
+    return fs.stat(filePath).then((stats) => {
+      if (stats.isDirectory()) {
+        return fs
+          .readdir(filePath)
+          .then((files) =>
+            Promise.all(
+              files.map((file) =>
+                this.ignoresGlobal.has(file)
+                  ? Promise.resolve()
+                  : this._walk(path.join(filePath, file), f)
+              )
+            )
+          );
+      } else if (filePath.toLowerCase().endsWith(".acutis")) {
+        return fs.readFile(filePath).then((src) => f(stats, filePath, src));
+      } else {
+        return Promise.resolve();
+      }
+    });
+  }
+};
+
+FileWalker.prototype.walk = function (f) {
+  return this._walk(this.root, f);
+};
+
+let defaultIgnores = new Set([".git"]);
+let globalIgnores = new Set(["node_modules"]);
 
 function forEachFiles(ignores, filePath, f) {
   if (ignores.has(filePath)) {
@@ -51,9 +94,9 @@ function acutisErrorToJsError(e) {
 }
 
 function getFuncs(obj) {
-  const result = [];
-  for (const key in obj) {
-    const f = obj[key];
+  let result = [];
+  for (let key in obj) {
+    let f = obj[key];
     if (typeof f === "function" && "interface" in f) {
       result.push({ key, f });
     }
@@ -78,21 +121,18 @@ module.exports = function (eleventyConfig, config) {
     read: false,
     init: function () {
       cache.clear();
-      const result =
+      let result =
         config && "components" in config
           ? getFuncs(config.components).map(({ key, f }) =>
               Component.funAsync(key, f.interface, f)
             )
           : [];
-      const ignores = defaultIgnores;
-      const dir = this.config.dir;
-      function f(_stats, filePath, src) {
-        result.push(Component.uint8Array(filePath, src));
-      }
-      return forEachFiles(ignores, path.join(dir.input, dir.includes), f)
-        .then(() => {
-          components = Compile.components(result);
-        })
+      let dir = this.config.dir;
+      return new FileWalker(path.join(dir.input, dir.includes))
+        .walk((_stats, filePath, src) =>
+          result.push(Component.uint8Array(filePath, src))
+        )
+        .then(() => (components = Compile.components(result)))
         .catch(acutisErrorToJsError);
     },
     compile: function (str, inputPath) {
@@ -105,7 +145,7 @@ module.exports = function (eleventyConfig, config) {
         } else {
           return Promise.resolve(str)
             .then((src) => {
-              const template = Compile.string(inputPath, components, src);
+              let template = Compile.string(inputPath, components, src);
               return function (data) {
                 return Render.async(template, data).catch(acutisErrorToJsError);
               };
@@ -118,7 +158,7 @@ module.exports = function (eleventyConfig, config) {
           template = fs
             .readFile(inputPath)
             .then((src) => {
-              const template = Compile.uint8Array(inputPath, components, src);
+              let template = Compile.uint8Array(inputPath, components, src);
               return function (data) {
                 return Render.async(template, data).catch(acutisErrorToJsError);
               };
@@ -180,7 +220,7 @@ RebuildOracle.prototype.addConfig = function (config) {
 RebuildOracle.prototype.addComponent = function (filePath, mtime) {
   this.currComponents.set(filePath, mtime);
   if (!this.shouldBuild) {
-    const prevMtime = this.prevComponents.get(filePath);
+    let prevMtime = this.prevComponents.get(filePath);
     this.shouldBuild = prevMtime ? mtime > prevMtime : true;
   }
 };
@@ -190,12 +230,12 @@ RebuildOracle.prototype.addTemplate = function (filePath, mtime) {
   if (this.shouldBuild) {
     return true;
   } else {
-    const prevMtime = this.prevTemplates.get(filePath);
+    let prevMtime = this.prevTemplates.get(filePath);
     return prevMtime ? mtime > prevMtime : true;
   }
 };
 
-const oracle = new RebuildOracle();
+let oracle = new RebuildOracle();
 
 // Before each Eleventy build, we need to rebuild any .acutis files which
 // changed.
@@ -204,41 +244,43 @@ module.exports.toJs = function (eleventyConfig, config) {
   eleventyConfig.versionCheck(">= 2.0");
   eleventyConfig.on("eleventy.before", ({ dir }) => {
     oracle.addConfig(config);
-    const compPath =
+    let compPath =
       config && "componentsPath" in config ? config.componentsPath : "";
-    const compFuns =
+    let compFuns =
       config && "components" in config ? getFuncs(config.components) : {};
-    const compSrc = [];
-    function f(stats, filePath, src) {
-      oracle.addComponent(filePath, stats.mtimeMs);
-      compSrc.push(Component.uint8Array(filePath, src));
-    }
-    const dirIncludes = path.join(dir.input, dir.includes);
-    const inputIgnores = new Set(defaultIgnores);
-    inputIgnores.add(dirIncludes);
-    inputIgnores.add(path.join(dir.input, dir.data));
-    inputIgnores.add(dir.output); // Not relative to input
-    return forEachFiles(defaultIgnores, dirIncludes, f)
+    let compSrc = [];
+    let dirIncludes = path.join(dir.input, dir.includes);
+    return new FileWalker(dirIncludes)
+      .walk((stats, filePath, src) => {
+        oracle.addComponent(filePath, stats.mtimeMs);
+        compSrc.push(Component.uint8Array(filePath, src));
+      })
       .then(() =>
-        forEachFiles(inputIgnores, dir.input, (stats, filePath, src) => {
-          const shouldBuild = oracle.addTemplate(filePath, stats.mtimeMs);
-          if (shouldBuild) {
-            const relativeCompPath =
-              "." + path.sep + path.relative(path.dirname(filePath), compPath);
-            const components = Compile.components(
-              compFuns
-                .map(({ key, f }) =>
-                  Component.funToJs(relativeCompPath, key, f.interface)
-                )
-                .concat(compSrc)
-            );
-            const template = Compile.uint8Array(filePath, components, src);
-            const js = Compile.toCJSString(template);
-            return fs.writeFile(filePath + ".js", js);
-          } else {
-            return Promise.resolve();
-          }
-        })
+        new FileWalker(dir.input)
+          .ignore(dirIncludes)
+          .ignore(path.join(dir.input, dir.data))
+          .ignore(dir.output) // Not relative to input
+          .walk((stats, filePath, src) => {
+            let shouldBuild = oracle.addTemplate(filePath, stats.mtimeMs);
+            if (shouldBuild) {
+              let relativeCompPath =
+                "." +
+                path.sep +
+                path.relative(path.dirname(filePath), compPath);
+              let components = Compile.components(
+                compFuns
+                  .map(({ key, f }) =>
+                    Component.funToJs(relativeCompPath, key, f.interface)
+                  )
+                  .concat(compSrc)
+              );
+              let template = Compile.uint8Array(filePath, components, src);
+              let js = Compile.toCJSString(template);
+              return fs.writeFile(filePath + ".js", js);
+            } else {
+              return Promise.resolve();
+            }
+          })
       )
       .then(() => oracle.reset())
       .catch(acutisErrorToJsError);
